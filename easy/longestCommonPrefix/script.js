e154407ad7d12244c99c5cc13fccd62996fd31b0
lam-brian <brianlam408@gmail.com>
2022-04-26 20:21:45 -0700

    complete longest common prefix

---
+++ easy/longestCommonPrefix/script.js
@@ -0,0 +1,20 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+const longestCommonPrefix = (strs) => {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+    }
+  }
+
+  return prefix;
+};
+
+const strs = ["flower", "flow", "flight"];
+
+console.log(longestCommonPrefix(strs));
