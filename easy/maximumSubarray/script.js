db68a26f57422f10f89855146c0361675a51af3f
lam-brian <brianlam408@gmail.com>
2022-04-28 15:59:37 -0700

    update maximum subarray

--- easy/maximumSubarray/script.js
+++ easy/maximumSubarray/script.js
@@ -4,15 +4,10 @@
  */
 var maxSubArray = function (nums) {
   let max = nums[0];
-  let curSum = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (curSum < 0) {
-      curSum = 0;
-    }
-    curSum += nums[i];
-    max = Math.max(max, curSum);
+  let currSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currSum = Math.max(currSum + nums[i], nums[i]);
+    max = Math.max(max, currSum);
   }
-
   return max;
 };
