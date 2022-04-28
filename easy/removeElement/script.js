efbb485a3d524e82d15ca1e566a7c5c476667ddb
lam-brian <brianlam408@gmail.com>
2022-04-27 19:00:00 -0700

    complete remove element challenge

---
+++ easy/removeElement/script.js
@@ -0,0 +1,10 @@
+var removeElement = function (nums, val) {
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] !== val) {
+      nums[left] = nums[right];
+      left++;
+    }
+  }
+  return left;
+};
