ce004417e2f3ab63ca1e165e6042a0ab1100085d
lam-brian <brianlam408@gmail.com>
2022-05-09 04:29:55 -0700

    redo plus one

--- easy/plusOne/script.js
+++ easy/plusOne/script.js
@@ -1,8 +1,16 @@
 var plusOne = function (digits) {
+  debugger;
   for (var i = digits.length - 1; i >= 0; i--) {
-    if (++digits[i] > 9) digits[i] = 0;
-    else return digits;
+    digits[i]++;
+    if (digits[i] > 9) {
+      digits[i] = 0;
+    } else {
+      return digits;
+    }
   }
   digits.unshift(1);
   return digits;
 };
+
+const digits = [9, 9, 9];
+console.log(plusOne(digits));
