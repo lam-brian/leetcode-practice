a39890a44cd23710325974501ab8bcf493e70807
lam-brian <brianlam408@gmail.com>
2022-05-17 13:56:35 -0700

    complete car fleet

---
+++ medium/carFleet/script.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function (target, position, speed) {
+  const cars = [];
+  const stack = [];
+  for (let i = 0; i < position.length; i++) {
+    cars.push([position[i], speed[i]]);
+  }
+  cars.sort((a, b) => b[0] - a[0]);
+  for (const [p, s] of cars) {
+    stack.push((target - p) / s);
+    if (
+      stack.length > 1 &&
+      stack[stack.length - 1] <= stack[stack.length - 2]
+    ) {
+      stack.pop();
+    }
+  }
+  return stack.length;
+};
