6957c113111ebfbaccf0c4f30dc86776aa2eb60f
lam-brian <brianlam408@gmail.com>
2022-05-18 16:06:20 -0700

    complete unique number of occurences

---
+++ easy/uniqueNumberOfOccurences/script.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+  const map = new Map();
+  const set = new Set();
+  for (const num of arr) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+  for (const [num, freq] of map) {
+    if (set.has(freq)) return false;
+    set.add(freq);
+  }
+  return true;
+};
