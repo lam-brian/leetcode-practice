5706309313983b69b040222a53a24cd006baaa29
lam-brian <brianlam408@gmail.com>
2022-05-20 17:46:50 -0700

    complete search a 2D Matrix

--- medium/searchA2DMatrix/script.js
+++ medium/searchA2DMatrix/script.js
@@ -6,25 +6,24 @@
 var searchMatrix = function (matrix, target) {
   const rows = matrix.length;
   const cols = matrix[0].length;
-
   let top = 0;
   let bot = rows - 1;
   while (top <= bot) {
-    let row = (top + bot) / 2;
-    if (target > matrix[row][cols - 1]) {
-      top = row + 1;
-    } else if (target < matrix[row][0]) {
-      bot = row - 1;
-    } else {
-      break;
-    }
+    let midRow = Math.floor((top + bot) / 2);
+    if (target > matrix[midRow][cols - 1]) {
+      top = midRow + 1;
+    } else if (target < matrix[midRow][0]) {
+      bot = midRow - 1;
+    } else break;
   }
+
   if (top > bot) return false;
-  let row = (top + bot) / 2;
+
+  let row = Math.floor((top + bot) / 2);
   let left = 0;
   let right = cols - 1;
   while (left <= right) {
-    let mid = (left + right) / 2;
+    let mid = Math.floor((left + right) / 2);
     if (target > matrix[row][mid]) {
       left = mid + 1;
     } else if (target < matrix[row][mid]) {
