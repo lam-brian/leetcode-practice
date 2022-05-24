8aaf14f4030fb559d7e393a007f863f332491b54
lam-brian <brianlam408@gmail.com>
2022-05-24 12:16:21 -0700

    complete search in rotated sorted array

--- medium/searchInRotatedSortedArray/script.js
+++ medium/searchInRotatedSortedArray/script.js
@@ -1,38 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var search = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
-
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
+    if (target === nums[mid]) return mid;
 
-    if (nums[mid] === target) {
-      return mid;
-    }
-
-    // When dividing the roated array into two halves, one must be sorted.
-
-    // Check if the left side is sorted
     if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target <= nums[mid]) {
-        // target is in the left
-        right = mid - 1;
-      } else {
-        // target is in the right
-        left = mid + 1;
-      }
-    }
-
-    // Otherwise, the right side is sorted
-    else {
-      if (nums[mid] <= target && target <= nums[right]) {
-        // target is in the right
-        left = mid + 1;
-      } else {
-        // target is in the left
-        right = mid - 1;
-      }
+      if (target > nums[mid] || target < nums[left]) left = mid + 1;
+      else right = mid - 1;
+    } else {
+      if (target < nums[mid] || target > nums[right]) right = mid - 1;
+      else left = mid + 1;
     }
   }
-
   return -1;
 };
