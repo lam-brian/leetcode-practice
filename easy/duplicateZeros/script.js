44c763a7697d10ab94c1725064a78b2968c0b0d6
lam-brian <brianlam408@gmail.com>
2022-05-30 18:41:37 -0700

    redo duplicate zeros

--- easy/duplicateZeros/script.js
+++ easy/duplicateZeros/script.js
@@ -3,11 +3,15 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function (arr) {
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] == 0) {
-      arr.splice(i, 0, 0);
-      arr.pop();
-      i++;
-    }
+  let size = arr.length;
+  let count = 0;
+  while (count < size) {
+    if (arr[0] === 0) arr.push(0);
+    const num = arr.shift();
+    arr.push(num);
+    count++;
+  }
+  while (arr.length > size) {
+    arr.pop();
   }
 };
