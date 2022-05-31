d9aeb99d4f12f9df62e49ca988918b8ea756303c
lam-brian <brianlam408@gmail.com>
2022-05-30 18:57:36 -0700

    redo max consecutive ones

--- easy/maxConsecutiveOnes/script.js
+++ easy/maxConsecutiveOnes/script.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) {
+  let maxLength = 0;
   let count = 0;
-  let maxLen = 0;
-
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1) count++;
-    if (nums[i + 1] !== 1 && count > maxLen) maxLen = count;
     if (nums[i] === 0) count = 0;
+    else {
+      count++;
+      maxLength = Math.max(maxLength, count);
+    }
   }
-
-  return maxLen;
+  return maxLength;
 };
