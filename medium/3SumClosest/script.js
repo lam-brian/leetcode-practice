faffba0663aaf2eb5c8f7dee7af75ec7367f8cf4
lam-brian <brianlam408@gmail.com>
2022-06-05 21:27:54 -0700

    complete 3 sum closest

---
+++ medium/3SumClosest/script.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+  let res = nums[0] + nums[1] + nums[2];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(sum - target) < Math.abs(res - target)) {
+        res = sum;
+      }
+      if (sum < target) left++;
+      else if (sum > target) right--;
+      else {
+        return sum;
+      }
+    }
+  }
+  return res;
+};
