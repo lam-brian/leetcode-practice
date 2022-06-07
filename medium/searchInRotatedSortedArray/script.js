8245bd114d63be3cf61c6f9b93fb3edda3cfbd0a
lam-brian <brianlam408@gmail.com>
2022-06-06 18:17:19 -0700

    redo search in rotated sorted array

--- medium/searchInRotatedSortedArray/script.js
+++ medium/searchInRotatedSortedArray/script.js
@@ -7,14 +7,15 @@ var search = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (target === nums[mid]) return mid;
+    const mid = Math.floor((left + right) / 2);
 
-    if (nums[left] <= nums[mid]) {
-      if (target > nums[mid] || target < nums[left]) left = mid + 1;
+    if (nums[mid] === target) return mid;
+
+    if (nums[mid] >= nums[left]) {
+      if (nums[mid] < target || nums[left] > target) left = mid + 1;
       else right = mid - 1;
     } else {
-      if (target < nums[mid] || target > nums[right]) right = mid - 1;
+      if (nums[mid] > target || nums[right] < target) right = mid - 1;
       else left = mid + 1;
     }
   }
