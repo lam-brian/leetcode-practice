2918ac266767630e41c6e04e394e217a08618fb0
lam-brian <brianlam408@gmail.com>
2022-06-17 17:21:03 -0700

    complete monotonic array

---
+++ easy/monotonicArray/script.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function (nums) {
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i + 1]) increasing = false;
+    if (nums[i] < nums[i + 1]) decreasing = false;
+    if (increasing === false && decreasing === false) return false;
+  }
+  return true;
+};
