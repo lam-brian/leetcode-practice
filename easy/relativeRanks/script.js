3bfba6dcc7b939cf486a7943744db712810a071f
lam-brian <brianlam408@gmail.com>
2022-06-22 17:59:48 -0700

    complete Relative Ranks

---
+++ easy/relativeRanks/script.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function (score) {
+  const sorted = [];
+  const res = new Array(score.length);
+  for (let i = 0; i < score.length; i++) {
+    sorted.push([i, score[i]]);
+  }
+  sorted.sort((a, b) => b[1] - a[1]);
+  let point = 1;
+  for (const [i, s] of sorted) {
+    let placement;
+    if (point === 1) {
+      placement = "Gold Medal";
+    } else if (point === 2) {
+      placement = "Silver Medal";
+    } else if (point === 3) {
+      placement = "Bronze Medal";
+    } else placement = point.toString();
+
+    res[i] = placement;
+    point++;
+  }
+  return res;
+};
