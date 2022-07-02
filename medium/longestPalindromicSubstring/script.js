d0c36991346d32ae03e43f3773445cdb67ee72ae
lam-brian <brianlam408@gmail.com>
2022-07-01 17:29:40 -0700

    complete longest palindromic substring

---
+++ medium/longestPalindromicSubstring/script.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var longestPalindrome = function (s) {
+  let res = "";
+  let length = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    // Odd length
+    let left = i;
+    let right = i;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > length) {
+        res = s.slice(left, right + 1);
+        length = right - left + 1;
+      }
+      left--;
+      right++;
+    }
+
+    // even
+    left = i;
+    right = i + 1;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > length) {
+        res = s.slice(left, right + 1);
+        length = right - left + 1;
+      }
+      left--;
+      right++;
+    }
+  }
+  return res;
+};
