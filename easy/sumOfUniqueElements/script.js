df4712174fd9fbcf5bd8d0a42d072f6b1dd4c644
lam-brian <brianlam408@gmail.com>
2022-07-05 17:00:10 -0700

    redo sum of unique elements

--- easy/sumOfUniqueElements/script.js
+++ easy/sumOfUniqueElements/script.js
@@ -3,14 +3,23 @@
  * @return {number}
  */
 var sumOfUnique = function (nums) {
-  const bucket = [];
+  const map = new Map();
   let res = 0;
   for (const num of nums) {
-    if (!bucket[num]) bucket[num] = [];
-    bucket[num].push(num);
+    map.set(num, (map.get(num) || 0) + 1);
   }
-  for (const num of bucket) {
-    if (num && num.length === 1) res += num[0];
+  for (const [num, freq] of map) {
+    if (freq === 1) res += num;
   }
   return res;
+  // const bucket = [];
+  // let res = 0;
+  // for (const num of nums) {
+  //   if (!bucket[num]) bucket[num] = [];
+  //   bucket[num].push(num);
+  // }
+  // for (const num of bucket) {
+  //   if (num && num.length === 1) res += num[0];
+  // }
+  // return res;
 };
