225aa4ffa17d6579573b1faaa585746214362b91
lam-brian <brianlam408@gmail.com>
2022-07-07 13:23:54 -0700

    complete first unique character in a string

---
+++ easy/firstUniqueCharacterInAString/script.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  var map = new Map();
+  for (i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], 2);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+
+  for (i = 0; i < s.length; i++) {
+    if (map.has(s[i]) && map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+};
