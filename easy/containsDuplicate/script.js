ba4596a2ebb3ea185e8cdeecda38e99fc3d5f53a
lam-brian <brianlam408@gmail.com>
2022-07-12 10:42:54 -0700

    redo contains duplicate

--- easy/containsDuplicate/script.js
+++ easy/containsDuplicate/script.js
@@ -1,9 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var containsDuplicate = function (nums) {
-  const storage = new Set();
-
-  for (let i = 0; i < nums.length; i++) {
-    if (storage.has(nums[i])) return true;
-    else storage.add(nums[i]);
+  const cache = {};
+  for (const num of nums) {
+    if (cache[num] !== undefined) return true;
+    cache[num] = num;
   }
   return false;
 };
