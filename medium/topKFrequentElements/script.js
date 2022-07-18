79e6da338b80c06c43b00e6d3543b8b4193594c8
lam-brian <brianlam408@gmail.com>
2022-07-18 14:12:54 -0700

    redo top k frequent elements

--- medium/topKFrequentElements/script.js
+++ medium/topKFrequentElements/script.js
@@ -1,19 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
 var topKFrequent = function (nums, k) {
+  const res = [];
   const freqMap = new Map();
   const bucket = [];
-  const result = [];
 
-  for (let num of nums) {
+  for (const num of nums) {
     freqMap.set(num, (freqMap.get(num) || 0) + 1);
   }
 
-  for (let [num, freq] of freqMap) {
+  for (const [num, freq] of freqMap) {
     bucket[freq] = (bucket[freq] || new Set()).add(num);
   }
 
   for (let i = bucket.length - 1; i >= 0; i--) {
-    if (bucket[i]) result.push(...bucket[i]);
-    if (result.length === k) break;
+    if (bucket[i]) res.push(...bucket[i]);
+    if (res.length === k) break;
   }
-  return result;
+  return res;
 };
