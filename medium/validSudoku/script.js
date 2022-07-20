95abf8b48260cc5ff0eb69081d55dc4e9cbe2208
lam-brian <brianlam408@gmail.com>
2022-07-20 16:38:54 -0700

    redo valid sudoku

--- medium/validSudoku/script.js
+++ medium/validSudoku/script.js
@@ -2,21 +2,23 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
+ var isValidSudoku = function(board) {
   for (let i = 0; i < 9; i++) {
-    let row = new Set(),
-      col = new Set(),
-      box = new Set();
-    for (let j = 0; j < 9; j++) {
-      let r = board[i][j];
-      let c = board[j][i];
-      let b =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-      if (row.has(r) || col.has(c) || box.has(b)) return false;
-      if (r !== ".") row.add(r);
-      if (c !== ".") col.add(c);
-      if (b !== ".") box.add(b);
-    }
+      const row = new Set();
+      const col = new Set();
+      const box = new Set();
+      for (let j = 0; j < 9; j++) {
+          const r = board[i][j];
+          const c = board[j][i];
+          const b = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
+          
+          if (row.has(r) || col.has(c) || box.has(b)) return false;
+          
+          if (r !== '.') row.add(r);
+          if (c !== '.') col.add(c);
+          if (b !== '.') box.add(b);
+      }
   }
   return true;
 };
+
