6481a850af073828b84a5227457bf487afbd736a
lam-brian <brianlam408@gmail.com>
2022-07-22 15:58:27 -0700

    redo valid palindrome

--- easy/validPalindrome/script.js
+++ easy/validPalindrome/script.js
@@ -2,23 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-
-const isAlphaNumeric = (c) => {
-  return (
-    ("A".charCodeAt() <= c.charCodeAt() &&
-      c.charCodeAt() <= "Z".charCodeAt()) ||
-    ("a".charCodeAt() <= c.charCodeAt() &&
-      c.charCodeAt() <= "z".charCodeAt()) ||
-    ("0".charCodeAt() <= c.charCodeAt() && c.charCodeAt() <= "9".charCodeAt())
-  );
-};
-
 var isPalindrome = function (s) {
+  const isAlphanumeric = (c) => {
+    return (
+      ("A".charCodeAt() <= c.charCodeAt() &&
+        c.charCodeAt() <= "Z".charCodeAt()) ||
+      ("a".charCodeAt() <= c.charCodeAt() &&
+        c.charCodeAt() <= "z".charCodeAt()) ||
+      ("0".charCodeAt() <= c.charCodeAt() && c.charCodeAt() <= "9".charCodeAt())
+    );
+  };
+
   let left = 0;
   let right = s.length - 1;
+
   while (left < right) {
-    while (left < right && !isAlphaNumeric(s[left])) left++;
-    while (left < right && !isAlphaNumeric(s[right])) right--;
+    while (left < right && !isAlphanumeric(s[left])) left++;
+    while (left < right && !isAlphanumeric(s[right])) right--;
+
     if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
     left++;
     right--;
