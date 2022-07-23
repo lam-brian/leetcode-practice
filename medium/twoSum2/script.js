21f55e32a940a27e416493e6a5cf2d87323d0256
lam-brian <brianlam408@gmail.com>
2022-07-23 14:34:13 -0700

    redo 2sum2

--- medium/twoSum2/script.js
+++ medium/twoSum2/script.js
@@ -3,13 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (numbers, target) {
+ var twoSum = function(numbers, target) {
   let left = 0;
   let right = numbers.length - 1;
   while (left < right) {
-    const sum = numbers[left] + numbers[right];
-    if (sum === target) return [left + 1, right + 1];
-    if (sum < target) left++;
-    else right--;
+      const sum = numbers[left] + numbers[right];
+      if (sum < target) left++;
+      else if (sum > target) right--;
+      else return [left + 1, right + 1];
   }
 };
