6e50fe31d13d54ea51da34b5dd9dbee4002b74e9
lam-brian <brianlam408@gmail.com>
2022-07-25 15:04:33 -0700

    redo container with most water

--- medium/containerWithMostWater/script.js
+++ medium/containerWithMostWater/script.js
@@ -1,16 +1,16 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
 var maxArea = function (height) {
-  // initalize 2 pointers at both ends
-  // calculate current area;
-  // if left is less than right ? i++ : r--;
-  let maxArea = 0;
+  let max = 0;
   let left = 0;
   let right = height.length - 1;
-
   while (left < right) {
     const area = Math.min(height[left], height[right]) * (right - left);
-    maxArea = Math.max(maxArea, area);
+    max = Math.max(max, area);
     if (height[left] < height[right]) left++;
     else right--;
   }
-  return maxArea;
+  return max;
 };
