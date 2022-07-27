6b3c70b485c6d00778f4eec601ad3822aa725410
lam-brian <brianlam408@gmail.com>
2022-07-27 14:45:34 -0700

    redo best time to buy and sell stock

--- easy/bestTimeToBuyAndSellStock/script.js
+++ easy/bestTimeToBuyAndSellStock/script.js
@@ -2,13 +2,12 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
+ var maxProfit = function(prices) {
   let maxP = 0;
-  let currP = 0;
-
-  for (let i = 1; i < prices.length; i++) {
-    currP = Math.max(0, currP + prices[i] - prices[i - 1]);
-    maxP = Math.max(maxP, currP);
+  let left = 0;
+  for (let right = 1; right < prices.length; right++) {
+      if (prices[left] > prices[right]) left = right;
+      maxP = Math.max(maxP, prices[right] - prices[left]);
   }
   return maxP;
 };
