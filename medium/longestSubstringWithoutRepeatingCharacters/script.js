4b24d4602df71b87f612468c94ea4e2b9a4bc750
lam-brian <brianlam408@gmail.com>
2022-07-28 16:41:14 -0700

    redo longest substring without repeating characters

--- medium/longestSubstringWithoutRepeatingCharacters/script.js
+++ medium/longestSubstringWithoutRepeatingCharacters/script.js
@@ -1,17 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLongestSubstring = function (s) {
-  const cache = new Set();
+  const countS = new Set();
+  let length = 0;
   let left = 0;
-  let result = 0;
   for (let right = 0; right < s.length; right++) {
-    while (cache.has(s[right])) {
-      cache.delete(s[left]);
+    while (countS.has(s[right])) {
+      countS.delete(s[left]);
       left++;
     }
-    cache.add(s[right]);
-    result = Math.max(result, right - left + 1);
+    countS.add(s[right]);
+    length = Math.max(length, right - left + 1);
   }
-  return result;
+  return length;
 };
-
-const s = "aab";
-console.log(lengthOfLongestSubstring(s));
