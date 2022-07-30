fee2103316c79241b4daf6edcb90a2eadf18fea4
lam-brian <brianlam408@gmail.com>
2022-07-30 16:44:51 -0700

    redo permutation in string

--- medium/permutationInString/script.js
+++ medium/permutationInString/script.js
@@ -1,32 +1,33 @@
-var checkInclusion = function (s1, s2) {
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+ var checkInclusion = function(s1, s2) {
   if (s1.length > s2.length) return false;
-
-  const s1Count = new Array(26).fill(0);
+  const countS1 = new Array(26).fill(0);
   const window = new Array(26).fill(0);
-
+  
   for (const char of s1) {
-    s1Count[char.charCodeAt() - "a".charCodeAt()]++;
+      const index = char.charCodeAt() - 'a'.charCodeAt();
+      countS1[index]++;
   }
-
   for (let i = 0; i < s1.length; i++) {
-    window[s2[i].charCodeAt() - "a".charCodeAt()]++;
+      const index = s2[i].charCodeAt() - 'a'.charCodeAt();
+      window[index]++
   }
-
   let start = 0;
   let end = s1.length - 1;
-
+  
   while (end < s2.length) {
-    if (window.join("") === s1Count.join("")) return true;
-    end++;
-    if (end === s2.length) break;
-    window[s2[start].charCodeAt() - "a".charCodeAt()]--;
-    window[s2[end].charCodeAt() - "a".charCodeAt()]++;
-    start++;
+      if (window.join('') === countS1.join('')) return true;
+      end++;
+      if (end === s2.length) break;
+      const startIndex = s2[start].charCodeAt() - 'a'.charCodeAt();
+      const endIndex = s2[end].charCodeAt() - 'a'.charCodeAt();
+      window[startIndex]--;
+      window[endIndex]++;
+      start++;
   }
   return false;
 };
-
-const s1 = "ab",
-  s2 = "eidbaooo";
-
-console.log(checkInclusion(s1, s2));
