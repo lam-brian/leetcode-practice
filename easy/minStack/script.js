787dd03e9eb1a66c84599cda18510ed8bfab7b5b
lam-brian <brianlam408@gmail.com>
2022-08-03 18:15:03 -0700

    redo min stack

--- easy/minStack/script.js
+++ easy/minStack/script.js
@@ -1,22 +1,44 @@
-class MinStack {
-  stack = [];
+var MinStack = function () {
+  this.stack = [];
+};
 
-  push(val) {
-    this.stack.push({
-      val,
-      min: this.stack.length === 0 ? val : Math.min(val, this.getMin()),
-    });
-  }
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push({
+    val: val,
+    min: this.stack.length === 0 ? val : Math.min(val, this.getMin()),
+  });
+};
 
-  pop() {
-    this.stack.pop();
-  }
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+};
 
-  top() {
-    return this.stack[this.stack.length - 1].val;
-  }
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1].val;
+};
 
-  getMin() {
-    return this.stack[this.stack.length - 1].min;
-  }
-}
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stack[this.stack.length - 1].min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
