14f8a712ea74febe338ba15ab5a3a68da608ab58
lam-brian <brianlam408@gmail.com>
2022-08-04 10:27:12 -0700

    redo evaluate reverse polish notation

--- medium/evaluateReversePolishNotation/script.js
+++ medium/evaluateReversePolishNotation/script.js
@@ -4,18 +4,16 @@
  */
 var evalRPN = function (tokens) {
   const stack = [];
-  for (let i = 0; i < tokens.length; i++) {
-    if (tokens[i] === "+") {
+  for (const token of tokens) {
+    if (token === "+") {
       stack.push(stack.pop() + stack.pop());
-    } else if (tokens[i] === "-") {
+    } else if (token === "-") {
       stack.push(-stack.pop() + stack.pop());
-    } else if (tokens[i] === "*") {
+    } else if (token === "*") {
       stack.push(stack.pop() * stack.pop());
-    } else if (tokens[i] === "/") {
+    } else if (token === "/") {
       stack.push(Math.trunc((1 / stack.pop()) * stack.pop()));
-    } else {
-      stack.push(parseInt(tokens[i]));
-    }
+    } else stack.push(parseInt(token));
   }
   return stack[0];
 };
