5b63b2e65b977ea311e5c25bb21ebf294c56a6b2
lam-brian <brianlam408@gmail.com>
2022-08-06 17:05:23 -0700

    redo daily temperatures

--- medium/dailyTemperatures/script.js
+++ medium/dailyTemperatures/script.js
@@ -2,16 +2,15 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function (temperatures) {
+ var dailyTemperatures = function(temperatures) {
   const res = new Array(temperatures.length).fill(0);
   const stack = [];
-
   for (let i = 0; i < temperatures.length; i++) {
-    while (temperatures[stack[stack.length - 1]] < temperatures[i]) {
-      const stackIndex = stack.pop();
-      res[stackIndex] = i - stackIndex;
-    }
-    stack.push(i);
+      while (stack.length && stack[stack.length - 1][1] < temperatures[i]) {
+          const [index, temp] = stack.pop();
+          res[index] = i - index;
+      }
+      stack.push([i, temperatures[i]]);
   }
   return res;
 };
