ac90602c70cbbb9c7538ae2e8ea8c954bd1c797a
lam-brian <brianlam408@gmail.com>
2022-08-12 16:09:18 -0700

    redo search a 2d matrix

--- medium/searchA2DMatrix/script.js
+++ medium/searchA2DMatrix/script.js
@@ -9,26 +9,20 @@ var searchMatrix = function (matrix, target) {
   let top = 0;
   let bot = rows - 1;
   while (top <= bot) {
-    let midRow = Math.floor((top + bot) / 2);
-    if (target > matrix[midRow][cols - 1]) {
-      top = midRow + 1;
-    } else if (target < matrix[midRow][0]) {
-      bot = midRow - 1;
-    } else break;
+    const midRow = Math.floor((top + bot) / 2);
+    if (matrix[midRow][cols - 1] < target) top = midRow + 1;
+    else if (matrix[midRow][0] > target) bot = midRow - 1;
+    else break;
   }
-
   if (top > bot) return false;
-
   let row = Math.floor((top + bot) / 2);
   let left = 0;
   let right = cols - 1;
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (target > matrix[row][mid]) {
-      left = mid + 1;
-    } else if (target < matrix[row][mid]) {
-      right = mid - 1;
-    } else return true;
+    const mid = Math.floor(left + (right - left) / 2);
+    if (matrix[row][mid] < target) left = mid + 1;
+    else if (matrix[row][mid] > target) right = mid - 1;
+    else return true;
   }
   return false;
 };
