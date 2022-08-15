67bc6129b4f85993054350efa7cd45edf715172c
Brian Lam <97998514+lam-brian@users.noreply.github.com>
2022-08-15 00:44:37 -0700

    redo koko eating bananas

--- medium/kokoEatingBananas/script.js
+++ medium/kokoEatingBananas/script.js
@@ -1,24 +1,25 @@
+
 /**
  * @param {number[]} piles
  * @param {number} h
  * @return {number}
  */
-var minEatingSpeed = function (piles, h) {
-  let left = 1;
-  let right = Math.max(...piles);
-  let res = right;
-  while (left <= right) {
-    let k = Math.floor((left + right) / 2);
-    let hours = 0;
-    for (const p of piles) {
-      hours += Math.ceil(p / k);
+var minEatingSpeed = function(piles, h) {
+    let left = 1;
+    let right = Math.max(...piles);
+    let res = right;
+    while (left <= right) {
+        let k = Math.floor((left + right) / 2);
+        let hours = 0;
+        for (const p of piles) {
+            hours += Math.ceil(p / k);
+        }
+        if (hours <= h) {
+            res = Math.min(res, k);
+            right = k - 1;
+        } else {
+            left = k + 1;
+        }
     }
-    if (hours <= h) {
-      res = Math.min(res, k);
-      right = k - 1;
-    } else {
-      left = k + 1;
-    }
-  }
-  return res;
+    return res;
 };
