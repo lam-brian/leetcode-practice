9b51482a5a264b7d909f6e777aa03b2ceda7e5c0
lam-brian <brianlam408@gmail.com>
2022-09-10 16:42:48 -0700

    redo single number

--- easy/singleNumber/script.js
+++ easy/singleNumber/script.js
@@ -3,10 +3,18 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
+  // using hashmap
   const cache = new Map();
   for (const num of nums) {
     if (cache.has(num)) cache.delete(num);
     else cache.set(num, num);
   }
   return [...cache.keys()][0];
+
+  // using bit manipulation
+  let res = 0;
+  for (const num of nums) {
+    res ^= num;
+  }
+  return res;
 };
