c5cf04ee7bd17fc3ed96adc0366849f713955055
lam-brian <brianlam408@gmail.com>
2022-09-12 18:11:46 -0700

    redo add two numbers

--- medium/addTwoNumbers/script.js
+++ medium/addTwoNumbers/script.js
@@ -11,21 +11,25 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  const before = new ListNode();
-  let tail = before;
-  let c = 0;
+  const dummy = new ListNode();
+  let cur = dummy;
+  let carry = 0;
 
-  while (l1 || l2 || c) {
+  while (l1 || l2 || carry) {
     const v1 = l1 ? l1.val : 0;
     const v2 = l2 ? l2.val : 0;
-    const v = v1 + v2 + c;
 
-    tail.next = new ListNode(v % 10);
-    tail = tail.next;
-    c = v >= 10 ? 1 : 0;
-    l1 = l1 && l1.next;
-    l2 = l2 && l2.next;
+    let val = v1 + v2 + carry;
+
+    carry = Math.floor(val / 10);
+    val = val % 10;
+
+    cur.next = new ListNode(val);
+
+    cur = cur.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
   }
 
-  return before.next;
+  return dummy.next;
 };
