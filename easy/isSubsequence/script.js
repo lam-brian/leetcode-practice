5fe890eba8fdb0c56ae51c32fe83b7ce04971931
lam-brian <brianlam408@gmail.com>
2022-10-04 18:31:10 -0700

    is subsequence

---
+++ easy/isSubsequence/script.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let pointerS = 0;
+  let pointerT = 0;
+
+  while (pointerS < s.length && pointerT < t.length) {
+    if (s[pointerS] === t[pointerT]) pointerS++;
+    pointerT++;
+  }
+
+  return pointerS === s.length;
+};
