aefeb6e929b1e549120a278a6c25595cca0779e6
lam-brian <brianlam408@gmail.com>
2022-10-05 16:51:08 -0700

    complete length of last word

---
+++ easy/lengthOfLastWord/script.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLastWord = function (s) {
+  let i = s.length - 1;
+  let length = 0;
+
+  while (s[i] === " ") {
+    i--;
+  }
+
+  while (i >= 0 && s[i] !== " ") {
+    length++;
+    i--;
+  }
+
+  return length;
+};
