263e9056d3c0fee6c72fb7782c40ae65c47155f0
lam-brian <brianlam408@gmail.com>
2022-10-07 11:51:07 -0700

    complete pascals triangle

---
+++ easy/pascalsTriangle/script.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  const res = [[1]];
+
+  for (let i = 0; i < numRows - 1; i++) {
+    const prevRow = res[res.length - 1];
+    const row = [1];
+    for (let j = 0; j < prevRow.length - 1; j++) {
+      row.push(prevRow[j] + prevRow[j + 1]);
+    }
+    row.push(1);
+    res.push(row);
+  }
+
+  return res;
+};
