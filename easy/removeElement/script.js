a01f91ff3fe55d3cf2aa4b965ce91f5f0673af9c
lam-brian <brianlam408@gmail.com>
2022-10-08 17:13:34 -0700

    redo remove element

--- easy/removeElement/script.js
+++ easy/removeElement/script.js
@@ -1,10 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
 var removeElement = function (nums, val) {
-  let left = 0;
-  for (let right = 0; right < nums.length; right++) {
-    if (nums[right] !== val) {
-      nums[left] = nums[right];
-      left++;
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
     }
   }
-  return left;
+
+  return k;
 };
