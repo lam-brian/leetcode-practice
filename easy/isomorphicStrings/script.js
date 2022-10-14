ed213dd17d4ff8146281c794821cf4b41de71f39
lam-brian <brianlam408@gmail.com>
2022-10-14 15:55:29 -0700

    complete isomorphic strings

---
+++ easy/isomorphicStrings/script.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  const mapST = {};
+  const mapTS = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const c1 = s[i];
+    const c2 = t[i];
+
+    if (
+      (c1 in mapST && mapST[c1] !== c2) ||
+      (c2 in mapTS && mapTS[c2] !== c1)
+    ) {
+      return false;
+    }
+
+    mapST[c1] = c2;
+    mapTS[c2] = c1;
+  }
+
+  return true;
+};
