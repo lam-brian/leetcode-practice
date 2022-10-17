fc65e9e2c67ea47c90a00846649b576c11a7d2fd
lam-brian <brianlam408@gmail.com>
2022-10-16 17:04:05 -0700

    redo majority element

--- easy/majorityElement/script.js
+++ easy/majorityElement/script.js
@@ -1,10 +1,7 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var majorityElement = function (nums) {
   let major = nums[0];
   let count = 1;
+
   for (let i = 1; i < nums.length; i++) {
     if (count === 0) {
       count++;
@@ -12,5 +9,6 @@ var majorityElement = function (nums) {
     } else if (major === nums[i]) count++;
     else count--;
   }
+
   return major;
 };
