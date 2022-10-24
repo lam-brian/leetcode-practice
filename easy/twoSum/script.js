cac98afa6fcd0caf67fa6bb41ecc0aed89b5d918
lam-brian <brianlam408@gmail.com>
2022-10-23 20:25:48 -0700

    redo two sum

--- easy/twoSum/script.js
+++ easy/twoSum/script.js
@@ -7,7 +7,7 @@ var twoSum = function (nums, target) {
   const cache = {};
   for (let i = 0; i < nums.length; i++) {
     const neededValue = target - nums[i];
-    if (cache[neededValue] !== undefined) return [cache[neededValue], i];
+    if (neededValue in cache) return [cache[neededValue], i];
     cache[nums[i]] = i;
   }
 };
