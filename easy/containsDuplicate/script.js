1c9523cf8af5f1d2cb64b968944c0a38052ffbd9
lam-brian <brianlam408@gmail.com>
2022-11-15 15:13:42 -0800

    redo contains duplicate

--- easy/containsDuplicate/script.js
+++ easy/containsDuplicate/script.js
@@ -4,9 +4,11 @@
  */
 var containsDuplicate = function (nums) {
   const cache = {};
+
   for (const num of nums) {
-    if (cache[num] !== undefined) return true;
+    if (num in cache) return true;
     cache[num] = num;
   }
+
   return false;
 };
