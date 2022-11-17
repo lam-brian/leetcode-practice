89bd93336cdaa9029adbd28c34011ff2a476cced
lam-brian <brianlam408@gmail.com>
2022-11-16 18:01:38 -0800

    redo valid anagram

--- easy/validAnagram/script.js
+++ easy/validAnagram/script.js
@@ -7,14 +7,16 @@ var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
 
   const countS = {};
+  const countT = {};
 
-  for (const char of s) {
-    countS[char] = (countS[char] || 0) + 1;
+  for (let i = 0; i < s.length; i++) {
+    countS[s[i]] = (countS[s[i]] || 0) + 1;
+    countT[t[i]] = (countT[t[i]] || 0) + 1;
   }
 
-  for (const char of t) {
-    if (!countS[char]) return false;
-    countS[char]--;
+  for (let i = 0; i < s.length; i++) {
+    if (countS[s[i]] !== countT[s[i]]) return false;
   }
+
   return true;
 };
