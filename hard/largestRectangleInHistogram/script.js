3785efc4285712e9d4f916d587d71dc9af9ad348
lam-brian <brianlam408@gmail.com>
2022-11-25 22:12:40 -0800

    redo largest rectangle in histogram

--- hard/largestRectangleInHistogram/script.js
+++ hard/largestRectangleInHistogram/script.js
@@ -6,14 +6,18 @@ var largestRectangleArea = function (heights) {
   heights.push(0);
   let res = 0;
   const stack = [];
+
   for (let i = 0; i < heights.length; i++) {
     let start = i;
+
     while (stack.length && stack[stack.length - 1][1] > heights[i]) {
       const [index, height] = stack.pop();
       res = Math.max(res, height * (i - index));
       start = index;
     }
+
     stack.push([start, heights[i]]);
   }
+
   return res;
 };
