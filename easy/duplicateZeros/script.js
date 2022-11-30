d93f5542712fb0738822d9191b65734f92726704
lam-brian <brianlam408@gmail.com>
2022-11-29 22:15:57 -0800

    redo duplicate zeros

--- easy/duplicateZeros/script.js
+++ easy/duplicateZeros/script.js
@@ -5,12 +5,17 @@
 var duplicateZeros = function (arr) {
   let size = arr.length;
   let count = 0;
+
   while (count < size) {
     if (arr[0] === 0) arr.push(0);
+
     const num = arr.shift();
+
     arr.push(num);
+
     count++;
   }
+
   while (arr.length > size) {
     arr.pop();
   }
