9e8aff46ebd65c5e6ced219a28ce79fdbfe4c2a7
lam-brian <brianlam408@gmail.com>
2022-11-30 21:25:01 -0800

    redo excel sheet column number

--- easy/excelSheetColumnNumber/script.js
+++ easy/excelSheetColumnNumber/script.js
@@ -4,8 +4,10 @@
  */
 var titleToNumber = function (columnTitle) {
   let columnNum = 0;
+
   for (let i = 0; i < columnTitle.length; i++) {
     let currentNum = columnTitle[i].charCodeAt() - "A".charCodeAt() + 1;
+
     columnNum += currentNum * Math.pow(26, columnTitle.length - 1 - i);
   }
   return columnNum;
