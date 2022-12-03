b0675b5062102038280af2f1f0b8cd2cddd45f99
Brian Lam <97998514+lam-brian@users.noreply.github.com>
2022-12-02 23:33:37 -0800

    redo pascal’s triangle

--- easy/pascalsTriangle/script.js
+++ easy/pascalsTriangle/script.js
@@ -8,9 +8,11 @@ var generate = function (numRows) {
   for (let i = 0; i < numRows - 1; i++) {
     const prevRow = res[res.length - 1];
     const row = [1];
+
     for (let j = 0; j < prevRow.length - 1; j++) {
       row.push(prevRow[j] + prevRow[j + 1]);
     }
+
     row.push(1);
     res.push(row);
   }
