f453a525aa059391c17430dc03731700db90b3dd
lam-brian <brianlam408@gmail.com>
2022-12-06 21:59:39 -0800

    redo find the difference;

--- easy/findTheDifference/script.js
+++ easy/findTheDifference/script.js
@@ -5,11 +5,14 @@
  */
 var findTheDifference = function (s, t) {
   let res = 0;
+
   for (const c of t) {
     res += c.charCodeAt();
   }
+
   for (const c of s) {
     res -= c.charCodeAt();
   }
+
   return String.fromCharCode(res);
 };
