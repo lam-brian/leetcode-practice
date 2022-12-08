c46b25764d931cc60f6354b64c8fec584e8dd454
lam-brian <brianlam408@gmail.com>
2022-12-07 17:04:47 -0800

    redo valid parentheses

--- easy/validParentheses/script.js
+++ easy/validParentheses/script.js
@@ -4,13 +4,16 @@
  */
 var isValid = function (s) {
   if (s.length <= 1) return false;
+
   const stack = [];
   const cache = { ")": "(", "]": "[", "}": "{" };
+
   for (const char of s) {
     if (char in cache) {
       if (stack[stack.length - 1] === cache[char]) stack.pop();
       else return false;
     } else stack.push(char);
   }
+
   return stack.length === 0;
 };
