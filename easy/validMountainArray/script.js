303dacf629fb7c9f067b4ea1bdef97da8eaf4ac1
lam-brian <brianlam408@gmail.com>
2022-12-08 19:21:25 -0800

    redo valid mountain array

--- easy/validMountainArray/script.js
+++ easy/validMountainArray/script.js
@@ -5,10 +5,12 @@
 var validMountainArray = function (arr) {
   let left = 0;
   let right = arr.length - 1;
+
   while (arr[left] < arr[left + 1] || arr[right] < arr[right - 1]) {
     if (arr[left] < arr[left + 1]) left++;
     if (arr[right] < arr[right - 1]) right--;
   }
+
   if (left !== right || left === arr.length - 1 || right === 0) return false;
   return true;
 };
