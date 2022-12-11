118a40b13f66c03952964276985e6360a51f95de
lam-brian <brianlam408@gmail.com>
2022-12-10 18:27:01 -0800

    redo first unique character in a string

--- easy/firstUniqueCharacterInAString/script.js
+++ easy/firstUniqueCharacterInAString/script.js
@@ -4,6 +4,7 @@
  */
 var firstUniqChar = function (s) {
   var map = new Map();
+
   for (i = 0; i < s.length; i++) {
     if (map.has(s[i])) {
       map.set(s[i], 2);
@@ -17,5 +18,6 @@ var firstUniqChar = function (s) {
       return i;
     }
   }
+
   return -1;
 };
