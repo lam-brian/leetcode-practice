c78b60595c2732a2c7fabbb2352f53447ec8cab0
lam-brian <brianlam408@gmail.com>
2022-12-18 21:39:10 -0800

    redo move zeroes

--- easy/moveZeroes/script.js
+++ easy/moveZeroes/script.js
@@ -4,11 +4,15 @@
  */
 var moveZeroes = function (nums) {
   let left = 0;
+
   for (let right = 0; right < nums.length; right++) {
     if (nums[right] !== 0) {
       let temp = nums[left];
+
       nums[left] = nums[right];
+
       nums[right] = temp;
+
       left++;
     }
   }
