531ece636e87ee4de53df16aecc055f3f58c2ed0
lam-brian <brianlam408@gmail.com>
2022-12-24 20:10:41 -0800

    search insert position

--- easy/searchInsertPosition/script.js
+++ easy/searchInsertPosition/script.js
@@ -5,8 +5,10 @@ var searchInsert = function (nums, target) {
 
   let left = 0;
   let right = nums.length - 1;
+
   while (left <= right) {
     const mid = Math.floor(left + (right - left) / 2);
+
     if (nums[mid] === target) return mid;
     else if (nums[mid] < target) {
       left = mid + 1;
@@ -14,5 +16,6 @@ var searchInsert = function (nums, target) {
       right = mid - 1;
     }
   }
+
   return left;
 };
