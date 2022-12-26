61d401f5baec23df881c44ee9111e2aaaab290ab
lam-brian <brianlam408@gmail.com>
2022-12-25 19:22:56 -0800

    redo trapping rain water

--- hard/trappingRainWater/script.js
+++ hard/trappingRainWater/script.js
@@ -8,14 +8,19 @@ var trap = function (height) {
   let right = height.length - 1;
   let leftMax = height[left];
   let rightMax = height[right];
+
   while (left < right) {
     if (leftMax < rightMax) {
       left++;
+
       leftMax = Math.max(leftMax, height[left]);
+
       res += leftMax - height[left];
     } else {
       right--;
+
       rightMax = Math.max(rightMax, height[right]);
+
       res += rightMax - height[right];
     }
   }
